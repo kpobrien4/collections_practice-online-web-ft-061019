fb980e0ba492ddf258864e561f935764c67aa44d
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-10 12:20:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,12 +29,12 @@ def kesha_maker(array)
   end
 end
 
-def find_a
+def find_a(array)
   array.find_all do |word|
     word[0] == "a"
 end
 
-def sum_array
+def sum_array(array)
   sum=0
   array.each do |num|
     sum+=num
